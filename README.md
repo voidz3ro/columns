7360a8ff8729a7ebcdc2fa03ab4b5a78c75bfbc4
Tristan Bullard <tristan.bullard@gmail.com>
2019-03-17 18:09:38 -0500

    made a sorter :)

--- README.md
+++ README.md
@@ -1,14 +1,12 @@
-# Columns
+# Welcome to 'Columns'
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.2.5.
+This project accepts words from a user and sorts the words alphabetically into four columns, vertically,
+then horizontally. The last row should be the only row that contains any empty cells if the number of words are not
+evenly divisible by 4.
 
-## Development server
+## Run it!
 
-Run `ng serve` for a dev server. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
-
-## Code scaffolding
-
-Run `ng generate component component-name` to generate a new component. You can also use `ng generate directive|pipe|service|class|guard|interface|enum|module`.
+Run `ng serve` to sort your words. Navigate to `http://localhost:4200/`. The app will automatically reload if you change any of the source files.
 
 ## Build
 
@@ -22,6 +20,3 @@ Run `ng test` to execute the unit tests via [Karma](https://karma-runner.github.
 
 Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protractortest.org/).
 
-## Further help
-
-To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Columns';
+  @ViewChild('') inputString: HTMLInputElement;
+  @Input() colCount = 4;
+  wordsArrSorted = [];
+  strVal = '';
+  columns = [];
+
+  constructor() { }
+
+  arrangeWords(items: Array<any>, numCols: number) {
+    this.columns = [];
+
+    const minPerRow = Math.floor(items.length / numCols);
+    const colCount = [];
+    const remaining = items.length % numCols;
+    const maxPerRow = Math.ceil(items.length / numCols);
+
+    for (let i = 0; i < numCols; i++) {
+      if (i < remaining && items[0] !== '') {
+        colCount.push(minPerRow + 1);
+      } else {
+        if (minPerRow === 0 && items.length > 0 && items[0] !== '') {
+          colCount.push(1);
+        } else {
+          colCount.push(minPerRow);
+        }
+      }
+    }
+
+    let count = 0;
+    for (let i = 0; i < colCount.length; i++) {
+      // create a new column
+      const column = {
+        cells: []
+      };
+
+      for (let j = 0; j < colCount[i]; j++) {
+        // add cells for this collection
+        const cell = {
+          word: items[count]
+        };
+        column.cells.push(cell);
+        count = count + 1;
+      }
+      this.columns.push(column);
+    }
+    // add dummies
+    this.columns.forEach(col => {
+      if (col.cells.length < maxPerRow) {
+        col.cells.push({ word: ' ' });
+      }
+    });
+  }
+
+  setWords() {
+    const _words = this.strVal.toLowerCase();
+    this.wordsArrSorted = _words.trim().split(/[ ,]+/).sort();
+    if (this.wordsArrSorted === ['']) {
+      this.wordsArrSorted = [];
+    }
+    this.arrangeWords(this.wordsArrSorted, this.colCount);
+  }
+
+  removeWord(cell: any, columnIndex: number, cellIndex: number) {
+    if (cell.word) {
+      this.strVal = this.strVal.toLowerCase().replace(cell.word.toLowerCase(), '').trim();
+    }
+    this.columns[columnIndex].cells.splice(cellIndex, 1);
+
+    if (this.strVal.trim() === '') {
+      this.columns = null;
+    } else {
+      this.setWords();
+    }
+  }
+
 }
